3ecdceb06f7b4be9c824f6880bc855deecc2b969
ReubenScott <ladderchin@gmail.com>
2025-04-03 00:21:41 +0900

    Android 7 の権限を追加

--- app/src/main/java/com/kindustry/market/db/DatabaseModule.kt
+++ app/src/main/java/com/kindustry/market/db/DatabaseModule.kt
@@ -19,15 +19,38 @@ object DatabaseModule {
     fun provideDatabase(
         @ApplicationContext
         context: Context
-    ) = Room.databaseBuilder(
-        context,
-        MarketDatabase::class.java,
-        "market"
-    )
-//      .createFromAsset("database/market.db") //  createFromAsset()方法，只能存取assets目錄下的檔案
-        .createFromFile(File("/storage/emulated/0/Studio/Data/market.db"))  // 直接從檔案開啟資料庫
-        .build()
+    ) : MarketDatabase {
+        // 在应用启动时，您可以检查外部文件的修改时间，并与应用私有数据目录下的数据库文件进行比较。
+        val externalDbFile = File("/storage/emulated/0/Studio/Data/market.db")
+        val internalDbFile = context.getDatabasePath("market")
 
+        if (externalDbFile.exists()) { // 检查外部文件是否存在
+            // if (externalDbFile.lastModified() > internalDbFile.lastModified())
+            // 清理/data目录下数据库，数据文件3套件 market， market-wal， market-shm
+            // /data/user/0/com.kindustry.market/databases
+//            internalDbFile.parentFile?.deleteRecursively()
+            // 复制外部数据库文件到内部存储
+            //  externalDbFile.copyTo(internalDbFile, overwrite = true)
+
+            // 从外部文件，创建新的Room数据库实例
+            return Room.databaseBuilder(
+                context,
+                MarketDatabase::class.java,
+                "market"
+            ).fallbackToDestructiveMigration()  // 强制关闭旧的数据库实例
+             .createFromFile(externalDbFile)    // 从外部文件创建数据库
+             .build()
+        } else {
+            // 使用APK发布包中assets目錄下的檔案，创建Room数据库实例
+            return Room.databaseBuilder(
+                context,
+                MarketDatabase::class.java,
+                "market"
+            ).fallbackToDestructiveMigration()
+            .createFromAsset("database/market.db") //  只能存取assets目錄下的檔案
+            .build()
+        }
+    }
 
     @Provides
     @Singleton
--- app/src/main/java/com/kindustry/market/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kindustry/market/ui/activity/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.kindustry.market.ui.activity
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -13,6 +16,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -31,7 +36,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestPermission(this) // 使用 Activity 的 Context
+        requestPermission(this, this) // 使用 Activity 的 Context
 
         //让内容，显示在状态栏和系统导航栏后面：状态栏和导航栏会遮盖部分内容
 //        WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -82,16 +87,51 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+
+
+    // 在 Activity 中处理权限请求结果
+/*    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_STORAGE_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 权限已授予
+                // 执行需要存储权限的操作
+            } else {
+                // 权限被拒绝
+                // 处理权限被拒绝的情况
+            }
+        }
+    }*/
+
 }
 
 //获取外部存储管理权限
-private fun requestPermission(context: Context) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R &&
-        !Environment.isExternalStorageManager()
-    ) {
-        val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(intent)
+private const val REQUEST_STORAGE_PERMISSION = 123 // 请求码
+fun requestPermission(context: Context, activity: Activity) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        // Android 11 及更高版本
+        if (!Environment.isExternalStorageManager()) {
+            val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+        }
+    } else {
+        // Android 10 及更低版本
+        val permissions = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        val permissionsToRequest = permissions.filter {
+            ContextCompat.checkSelfPermission(context, it) != PackageManager.PERMISSION_GRANTED
+        }.toTypedArray()
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(activity, permissionsToRequest, REQUEST_STORAGE_PERMISSION)
+        }
     }
 }
-
